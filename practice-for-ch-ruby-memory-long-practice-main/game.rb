118962d214cc659d622ed3c119571bba7a7bb128
XLG123 <xiaolin.guan123@gmail.com>
2023-05-16 16:52:27 -0400

    Game Class in progress

--- practice-for-ch-ruby-memory-long-practice-main/game.rb
+++ practice-for-ch-ruby-memory-long-practice-main/game.rb
@@ -8,33 +8,49 @@ class Game
   end
 
   def get_user_input
-    row, col = gets.chomp.split.map! { |idx| idx.to_i }
+    gets.chomp.split.map! { |idx| idx.to_i }
   end
 
   def play
-    @board.render
+    # @board.render
 
     pos = self.get_user_input
 
-    @guessed_pos = pos if !@guessed_pos
+    card_1 = @board[*pos]
+    if !card_1.to_s.empty?
+      return nil
+    end
+    card_1.reveal
 
-    card = @board[*pos]
+    # @board.render
 
-  
-
-    #...
-    if @guessed_pos
-      @guessed_pos = nil
-    else
+    if !@guessed_pos
       @guessed_pos = pos
+      @board.render
+    else
+      card_2 = @board[*@guessed_pos]
+      card_2.reveal
+      @board.render
+      if card_1 != card_2
+        card_1.hide
+        card_2.hide
+        @board.render
+      end
+      @guessed_pos = nil
     end
   end
 
-  def game
-    unless @board.won?
-      @board.render
+  # def clear
 
-      self.get_user_input
+  # end
+
+  def game
+    @board.populate
+    p @board
+
+    @board.render
+    while !@board.won?
+      play
     end
   end
 end
